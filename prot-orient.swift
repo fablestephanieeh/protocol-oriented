39212f72c45c272d3699bab0cd6164e587cec364
fablestephanieeh <138759993+fablestephanieeh@users.noreply.github.com>
2023-07-20 14:59:38 +0700

    Create prot-orient.swift

---
+++ prot-orient.swift
@@ -0,0 +1,37 @@
+protocol CanFly {
+    func fly()
+}
+
+class Bird {
+    var isFemale = true
+    
+    func layEgg() {
+        if isFemale {
+            print("The bird makes a new bird in a shell.")
+        }
+    }
+}
+
+class Eagle: Bird, CanFly {
+    func fly() {
+        print("The eagle flaps its wings and lifts off into the sky.")
+    }
+}
+
+class Penguin: Bird {
+    func swim() {
+        print("The penguin paddles through the water.")
+    }
+}
+
+struct FlyingMuseum {
+    func flyingDemo(flyingObject: CanFly) {
+        flyingObject.fly()
+    }
+}
+
+let myEagle = Eagle()
+let myPenguin = Penguin()
+let myMuseum = FlyingMuseum()
+
+myMuseum.flyingDemo(flyingObject: myEagle)
